40caf446f40a5501a5c1bee4b96745c922bcead8
DiscourteousPaella82 <c.sheridan30@universityofgalway.ie>
2025-06-27 11:47:55 +0100

    Added basic java callable threads. New package with a threadClient which handles methods related comments now.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,26 +3,20 @@ package org.example;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.sql.Connection;
-import java.util.ArrayList;
 import java.util.List;
 
-import comment.CommentClient;
 import comment.CommentThreadData;
 import database.DatabaseFunctions;
+import thread.ThreadClient;
 import video.Video;
 import video.VideoClient;
 import youtube.YoutubeClient;
 
 import com.google.api.services.youtube.YouTube;
 
-
-public class Main {
+public class Main{
     public static void main(String[] args) throws GeneralSecurityException, IOException {
-
         YouTube youTube = YoutubeClient.getService();
-
-        int numberComments = 0;
-        int requestCount = 0;
 
         try {    
             DatabaseFunctions databaseFunctions = new DatabaseFunctions();
@@ -32,32 +26,10 @@ public class Main {
             
             VideoClient videoClient = new VideoClient(youTube);
 
-            requestCount ++;
             List<Video> videoList = videoClient.getMostPopularVideos();
 
-            CommentClient commentClient = new CommentClient(youTube);
-
-            List<CommentThreadData> commentThreadData = new ArrayList<>();
-
-            for(int i = 0; i < videoList.size(); i++) {
-
-                requestCount ++;
-                commentThreadData.addAll(commentClient.findCommentThreadByVideoId(
-                    videoList.get(i).id()));
-                
-                for (int j = 0; j < commentThreadData.size(); j++) {
-                    System.out.println(commentThreadData.get(j));
-                }
-
-                for (CommentThreadData commentThreadData1 : commentThreadData) {
-                    numberComments++;
-                    if (commentThreadData1.commentReplies() != null)
-                        numberComments += commentThreadData1.commentReplies().size();
-                }
-
-            }
-            System.out.println("\n"+commentThreadData.size() + " comment threads captured.\n"
-                + numberComments + " total comments captured." + "\n" + requestCount + " requests made.");
+            ThreadClient threadClient = new ThreadClient();
+            List<CommentThreadData> commentThreadData = threadClient.requestCommentThreadData(youTube, videoList);
 
             databaseFunctions.insertIntoCommentTable(connection, commentThreadData);
             connection.close();
---
+++ src/main/java/thread/ThreadClient.java
@@ -0,0 +1,83 @@
+package thread;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import com.google.api.services.youtube.YouTube;
+
+import comment.CommentClient;
+import comment.CommentThreadData;
+import video.Video;
+
+public class ThreadClient {
+
+    private int commentCount;
+    private int commentThreadCount;
+    private int commentRequestCount;
+
+    public List<CommentThreadData> requestCommentThreadData(YouTube youTube,List<Video> videoList){
+        commentCount = 0;
+        commentRequestCount = 0;
+
+        long start = System.currentTimeMillis();
+        ExecutorService executor = Executors.newFixedThreadPool(100);
+        
+        List<Future<List<CommentThreadData>>> futureList = new ArrayList<Future<List<CommentThreadData>>>();
+        List<Callable<List<CommentThreadData>>> callables = new ArrayList<Callable<List<CommentThreadData>>>();
+
+        List<CommentThreadData> commentThreadDataList = new ArrayList<CommentThreadData>();
+
+        try{
+            for(Video video:videoList){
+                callables.add(new CommentClient(youTube, video.id()));
+            }
+
+            futureList = executor.invokeAll(callables);
+            commentRequestCount += callables.size();
+
+            for(Future<List<CommentThreadData>> future : futureList){
+                try {
+                    commentThreadDataList.addAll(future.get());
+                    System.out.println(new Date()+ "::"+commentThreadDataList);
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        executor.shutdown();
+
+        commentThreadCount = commentThreadDataList.size();
+
+        commentCount += commentThreadCount;
+        for(CommentThreadData commentThreadData: commentThreadDataList){
+            commentCount += commentThreadData.commentReplies().size();
+        }
+
+        System.out.println("Number of comment requests made: " + commentRequestCount + "\nNumber of commentThreads received: " + commentThreadCount + "\n Number of comments received: " + commentCount + "\nTime taken: " + timeElapsed + "ms");
+
+        return commentThreadDataList;
+    }
+    
+    public int getCommentCount(){
+        return commentCount;
+    }
+    
+    public int getCommentThreadCount(){
+        return commentThreadCount;
+    }    
+    
+    public int getCommentRequestCount(){
+        return commentRequestCount;
+    }
+}
